99dc4392c033ae85e8b0136cedd3c0adb4b56be1
Aznes <timotheefageot@outlook.fr>
2025-05-23 15:45:31 +0200

    particles moving

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include "opengl-framework/opengl-framework.hpp"
 #include "utils.hpp"
 
-int main()
-{
+struct Particle {
+    glm::vec2 position;
+    glm::vec2 direction;
+    float speed;
+    float mass;
+};
+
+int main() {
     gl::init("Particules!");
     gl::maximize_window();
     glEnable(GL_BLEND);
@@ -10,12 +16,50 @@ int main()
 
     // TODO: create an array of particles
 
-    while (gl::window_is_open())
-    {
+    std::vector<Particle> particles;
+    for (size_t i = 0; i < 100; i++) {
+        float minPos = -1.0f;
+        float maxPos = 1.0f;
+        float minSpeed = 0.1f;
+        float maxSpeed = 0.5f;
+
+        Particle particle;
+        particle.position = glm::vec2(utils::rand(minPos, maxPos), utils::rand(minPos, maxPos));
+        particle.direction = glm::vec2(utils::rand(minPos, maxPos), utils::rand(minPos, maxPos));
+        particle.speed = utils::rand(minSpeed, minSpeed);
+        particle.mass = 0.1f;
+        particles.push_back(particle);
+    }
+
+    while (gl::window_is_open()) {
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         // TODO update particles
+
         // TODO render particles
+
+        for (size_t i = 0; i < particles.size(); i++) {
+            glm::vec2 normalizedDirection = glm::normalize(particles[i].direction);
+
+            // Check for collision with the window borders
+            particles[i].direction = normalizedDirection;
+            particles[i].position += particles[i].speed * normalizedDirection * gl::delta_time_in_seconds();
+            //particles[i].position.y += particles[i].mass * -9.81f * gl::delta_time_in_seconds();
+
+            // Check for collision with the window borders
+            if (particles[i].position.x < -1.0f || particles[i].position.x > 1.0f) {
+                particles[i].direction.x *= -1.0f;
+            }
+            if (particles[i].position.y < -1.0f || particles[i].position.y > 1.0f) {
+                particles[i].direction.y *= -1.0f;
+            }
+
+            // Update position
+            particles[i].position += particles[i].speed * particles[i].direction * gl::delta_time_in_seconds();
+
+            //Draw the particle
+            utils::draw_disk(particles[i].position, 0.05f, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+        }
     }
 }
