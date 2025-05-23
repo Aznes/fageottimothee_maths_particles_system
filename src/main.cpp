64f13d686b543a10257cc14533f8951cee124aa5
Aznes <timotheefageot@outlook.fr>
2025-05-23 16:15:28 +0200

    air friction

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,11 @@
 
 struct Particle {
     glm::vec2 position;
-    glm::vec2 direction;
-    float speed;
+    glm::vec2 velocity;
+    float radius = 0.05f;
+    // float speed;
     float mass;
+    float friction = 0.1f; // Friction coefficient
 };
 
 int main() {
@@ -25,11 +27,13 @@ int main() {
 
         Particle particle;
         particle.position = glm::vec2(utils::rand(minPos, maxPos), utils::rand(minPos, maxPos));
-        particle.direction = glm::vec2(utils::rand(minPos, maxPos), utils::rand(minPos, maxPos));
-        particle.speed = utils::rand(minSpeed, minSpeed);
+        particle.velocity = glm::vec2(utils::rand(minPos, maxPos), utils::rand(minPos, maxPos));
+        // particle.speed = utils::rand(minSpeed, minSpeed);
         particle.mass = 0.1f;
         particles.push_back(particle);
     }
+
+    float airDynamicViscosity = 4.f;
 
     while (gl::window_is_open()) {
         glClearColor(0.f, 0.f, 0.f, 1.f);
@@ -40,26 +44,35 @@ int main() {
         // TODO render particles
 
         for (size_t i = 0; i < particles.size(); i++) {
-            glm::vec2 normalizedDirection = glm::normalize(particles[i].direction);
+            // glm::vec2 normalizedDirection = glm::normalize(particles[i].direction);
 
             // Check for collision with the window borders
-            particles[i].direction = normalizedDirection;
-            particles[i].position += particles[i].speed * normalizedDirection * gl::delta_time_in_seconds();
-            particles[i].position.y += particles[i].mass * -9.81f * gl::delta_time_in_seconds();
+            //particles[i].direction = normalizedDirection;
+            // particles[i].speed = -3.0f * 3.14f * airDynamicViscosity * particles[i].radius * particles[i].speed;
+            glm::vec2 forces(0.f, 0.f);
+            //forces += particles[i].mass * 9.81f * glm::vec2(0.f, -1.f); // Gravity
+            forces +=  - airDynamicViscosity * particles[i].radius * particles[i].velocity;
 
-            // Check for collision with the window borders
-            if (particles[i].position.x < -1.0f || particles[i].position.x > 1.0f) {
-                particles[i].direction.x *= -1.0f;
+            particles[i].velocity += forces * gl::delta_time_in_seconds() / particles[i].mass;
+            particles[i].position += particles[i].velocity * gl::delta_time_in_seconds();
+
+            if(particles[i].position.y < -1.0f) {
+                particles[i].position.y = 1.0f;
             }
-            if (particles[i].position.y < -1.0f || particles[i].position.y > 1.0f) {
-                particles[i].direction.y *= -1.0f;
-            }
+
+            // // Check for collision with the window borders
+            // if (particles[i].position.x < -1.0f || particles[i].position.x > 1.0f) {
+            //     particles[i].direction.x *= -1.0f;
+            // }
+            // if (particles[i].position.y < -1.0f || particles[i].position.y > 1.0f) {
+            //     particles[i].direction.y *= -1.0f;
+            // }
 
             // Update position
-            particles[i].position += particles[i].speed * particles[i].direction * gl::delta_time_in_seconds();
+            // particles[i].position += particles[i].speed * particles[i].direction * gl::delta_time_in_seconds();
 
             //Draw the particle
-            utils::draw_disk(particles[i].position, 0.05f, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
+            utils::draw_disk(particles[i].position, particles[i].radius, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
         }
     }
 }
