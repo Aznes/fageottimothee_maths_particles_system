b459bb1507673cc82794efd84ad94ad82fe9cd6a
Aznes <timotheefageot@outlook.fr>
2025-06-20 16:03:41 +0200

    multiple walls

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,11 @@ struct Particle {
     glm::vec4 color = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
 };
 
+struct Wall {
+    glm::vec2 A;
+    glm::vec2 B;
+};
+
 int main() {
     gl::init("Particules!");
     gl::maximize_window();
@@ -25,7 +30,7 @@ int main() {
     // TODO: create an array of particles
 
     std::vector<Particle> particles;
-    for (size_t i = 0; i < 100; i++) {
+    for (size_t i = 0; i < 1; i++) {
         float minPos = -1.0f;
         float maxPos = 1.0f;
         float minSpeed = 0.1f;
@@ -45,6 +50,14 @@ int main() {
         particles.push_back(particle);
     }
 
+    std::vector<Wall> walls;
+    for (size_t i = 0; i < 4; i++) {
+        Wall wall;
+        wall.A = glm::vec2(utils::rand(-1.0f, 1.0f), utils::rand(-1.0f, 1.0f));
+        wall.B = glm::vec2(utils::rand(-1.0f, 1.0f), utils::rand(-1.0f, 1.0f));
+        walls.push_back(wall);
+    }
+
     float airDynamicViscosity = 4.f;
 
     while (gl::window_is_open()) {
@@ -55,36 +68,35 @@ int main() {
 
         // TODO render particles
 
-        // First wall
-        glm::vec2 A(-1.0f, 0.0f);
-        glm::vec2 B( 1.0f, 0.0f);
-        utils::draw_line(A, B, 0.01f, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-
         // Second wall
         glm::vec2 C( 0.0f,-1.0f);
         glm::vec2 D( gl::mouse_position());
         utils::draw_line(C, D, 0.01f, glm::vec4(0.0f, 1.0f, 1.0f, 1.0f));
 
-        // Cross check
-        glm::vec2 r = B - A;
-        glm::vec2 s = D - C;
+        for (const Wall& wall : walls) {
+            utils::draw_line(wall.A, wall.B, 0.01f, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
 
-        glm::mat2 M(r, -s);    
+            // Cross check
+            glm::vec2 r = wall.B - wall.A;
+            glm::vec2 s = D - C;
 
-        glm::vec2 v = C - A;
+            glm::mat2 M(r, -s);    
 
-        float det = glm::determinant(M);
+            glm::vec2 v = C - wall.A;
 
-        if (fabs(det) > 1e-6f) {
-            glm::vec2 t = glm::inverse(M) * v;
+            float det = glm::determinant(M);
 
-            float u = t.x;
-            float v = t.y;
+            if (fabs(det) > 1e-6f) {
+                glm::vec2 t = glm::inverse(M) * v;
 
-            if (u >= 0.0f && u <= 1.0f && v >= 0.0f && v <= 1.0f) {
-                // crossing point
-                glm::vec2 P = A + u * r;
-                utils::draw_disk(P, 0.05f, glm::vec4(0.0f,1.0f,0.0f,1.0f));
+                float u = t.x;
+                float v = t.y;
+
+                if (u >= 0.0f && u <= 1.0f && v >= 0.0f && v <= 1.0f) {
+                    // crossing point
+                    glm::vec2 P = wall.A + u * r;
+                    utils::draw_disk(P, 0.05f, glm::vec4(0.0f,1.0f,0.0f,1.0f));
+                }
             }
         }
 
@@ -93,7 +105,7 @@ int main() {
         }
 
         for (size_t i = 0; i < particles.size(); i++) {
-            particles[i].age += gl::delta_time_in_seconds();
+            //particles[i].age += gl::delta_time_in_seconds();
 
             // float t = glm::clamp(particles[i].age / particles[i].lifetime, 0.0f, 1.0f);
             // particles[i].radius = particles[i].initialRadius * (1.0f - t);
@@ -120,7 +132,8 @@ int main() {
             
             glm::vec2 forces(0.f, 0.f);
             //forces += particles[i].mass * 9.81f * glm::vec2(0.f, -1.f); // Gravity
-            forces +=  - airDynamicViscosity * particles[i].radius * particles[i].velocity; //Friction
+            //forces +=  - airDynamicViscosity * particles[i].radius * particles[i].velocity; //Friction
+            forces += particles[i].radius * particles[i].velocity; //Friction
             //forces += gl::mouse_position();
 
             particles[i].velocity += forces * gl::delta_time_in_seconds() / particles[i].mass;
@@ -134,20 +147,46 @@ int main() {
             // }
 
             // Check for collision with the window borders
-            if (particles[i].position.x < -1.0f || particles[i].position.x > 1.0f) {
-                particles[i].velocity.x = -particles[i].velocity.x;
-                particles[i].position.x = glm::clamp(particles[i].position.x, -1.0f, 1.0f);
-            }
-            if (particles[i].position.y < -1.0f || particles[i].position.y > 1.0f) {
-                particles[i].velocity.y = -particles[i].velocity.y;
-                particles[i].position.y = glm::clamp(particles[i].position.y, -1.0f, 1.0f);
-            }
+            // if (particles[i].position.x < -1.0f || particles[i].position.x > 1.0f) {
+            //     particles[i].velocity.x = -particles[i].velocity.x;
+            //     particles[i].position.x = glm::clamp(particles[i].position.x, -1.0f, 1.0f);
+            // }
+            // if (particles[i].position.y < -1.0f || particles[i].position.y > 1.0f) {
+            //     particles[i].velocity.y = -particles[i].velocity.y;
+            //     particles[i].position.y = glm::clamp(particles[i].position.y, -1.0f, 1.0f);
+            // }
 
             // Update position
             // particles[i].position += particles[i].speed * particles[i].direction * gl::delta_time_in_seconds();
 
             //Draw the particle
             utils::draw_disk(particles[i].position, particles[i].radius, particles[i].color);
+
+            // glm::vec2 particlePosition( particles[i].position.x, particles[i].position.y);
+
+            // // Cross check
+            // glm::vec2 r = B - A;
+            // glm::vec2 s = particlePosition - particlePosition;
+
+            // glm::mat2 M(r, -s);    
+
+            // glm::vec2 v = particlePosition - A;
+
+            // float det = glm::determinant(M);
+
+            // if (fabs(det) > 1e-6f) {
+            //     glm::vec2 t = glm::inverse(M) * v;
+
+            //     float u = t.x;
+            //     float v = t.y;
+
+            //     if (u >= 0.0f && u <= 1.0f && v >= 0.0f && v <= 1.0f) {
+            //         // crossing point
+            //         glm::vec2 P = A + u * r;
+            //         utils::draw_disk(P, 0.05f, glm::vec4(0.0f,1.0f,0.0f,1.0f));
+            //         particles[i].velocity = glm::reflect(particles[i].velocity, glm::normalize(B - A));
+            //     }
+            // }
         }
     }
 }
