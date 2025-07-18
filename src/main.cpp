c9b10f62c82a95a2c116a1d8bd9b0f6891587281
Aznes <timotheefageot@outlook.fr>
2025-07-18 10:52:58 +0200

    DeCasteljau bezizer curves!

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,20 @@ float circleRadius  = 0.5f;
 int segmentsAmount = 100;
 float segmentsThickness = 0.01f;
 
-glm::vec2 bezier3(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& p2, const glm::vec2& p3, float t) {
-    float u = 1.0f - t;
-    float tt = t * t;
-    float uu = u * u;
-    float uuu = uu * u;
-    float ttt = tt * t;
+glm::vec2 deCasteljau1(const glm::vec2& p0, const glm::vec2& p1, float t) {
+    return (1 - t) * p0 + t * p1;
+}
 
-    glm::vec2 p = uuu * p0;
-    p += 3 * uu * t * p1;
-    p += 3 * u * tt * p2;   
-    p += ttt * p3;
+glm::vec2 deCasteljau2(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& p2, float t) {
+    glm::vec2 p01 = deCasteljau1(p0, p1, t);
+    glm::vec2 p12 = deCasteljau1(p1, p2, t);
+    return deCasteljau1(p01, p12, t);
+}
 
-    return p;
+glm::vec2 deCasteljau3(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& p2, const glm::vec2& p3, float t) {
+    glm::vec2 p012 = deCasteljau2(p0, p1, p2, t);
+    glm::vec2 p123 = deCasteljau2(p1, p2, p3, t);
+    return deCasteljau1(p012, p123, t);
 }
 
 void draw_parametric(std::function<glm::vec2(float)> const& parametric)
@@ -43,19 +44,18 @@ int main() {
     gl::init("Particules!");
     gl::maximize_window();
 
+    glm::vec2 l0(-0.8f, 0.8f), l1(0.8f, 0.8f);
+
+    glm::vec2 q0(-0.8f,  0.0f), q1(0.0f, 0.8f), q2(0.8f, 0.0f);
+    
+    glm::vec2 c0(-0.8f, -0.8f), c1(-0.4f, 0.8f), c2(0.4f, -0.8f), c3(0.8f, -0.2f);
+
     while (gl::window_is_open()) {
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
     
-        draw_parametric([](float t) {
-            float angle = 2.0f * PI * t;
-            float x = 16.0f * std::pow(std::sin(angle), 3);
-            float y = 13.0f * std::cos(angle)
-                    -  5.0f * std::cos(2.0f * angle)
-                    -  2.0f * std::cos(3.0f * angle)
-                    -      std::cos(4.0f * angle);
-            glm::vec2 p = glm::vec2(x, y) * (circleRadius / 16.0f);
-            return circleCenterPosition + p;
-        });
+        draw_parametric([&](float t) { return deCasteljau1(l0, l1, t); });
+        draw_parametric([&](float t) { return deCasteljau2(q0, q1, q2, t); });
+        draw_parametric([&](float t) { return deCasteljau3(c0, c1, c2, c3, t); });
     }
 }
