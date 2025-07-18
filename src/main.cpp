5db97920d5938a801d02f63221fa545ff4b86d4e
Aznes <timotheefageot@outlook.fr>
2025-07-18 11:07:21 +0200

    Mouse position controled curves!

--- src/main.cpp
+++ src/main.cpp
@@ -26,8 +26,7 @@ glm::vec2 bernstein3(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& 
     return p0 * (u * u * u) + p1 * (3 * u * u * t) + p2 * (3 * u * t * t) + p3 * (t * t * t);
 }
 
-void draw_parametric(std::function<glm::vec2(float)> const& parametric)
-{
+void draw_parametric(std::function<glm::vec2(float)> const& parametric){
     glm::vec2 prev = parametric(0.0f);
 
     for (int i = 1; i <= segmentsAmount; ++i) {
@@ -42,17 +41,14 @@ int main() {
     gl::init("Particules!");
     gl::maximize_window();
 
-    glm::vec2 l0(-0.8f, 0.8f), l1(0.8f, 0.8f);
-
-    glm::vec2 q0(-0.8f,  0.0f), q1(0.0f, 0.8f), q2(0.8f, 0.0f);
-
-    glm::vec2 c0(-0.8f, -0.8f), c1(-0.4f, 0.8f), c2(0.4f, -0.8f), c3(0.8f, -0.2f);
-
     while (gl::window_is_open()) {
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
     
-        draw_parametric([&](float t) { return bernstein1(l0, l1, t); });
+        glm::vec2 q0(-0.8f,  0.0f), q1(gl::mouse_position()), q2(0.8f, 0.0f);
+
+        glm::vec2 c0(-0.8f, -0.8f), c1(-0.4f, 0.8f), c2(gl::mouse_position()), c3(0.8f, -0.2f);
+
         draw_parametric([&](float t) { return bernstein2(q0, q1, q2, t); });
         draw_parametric([&](float t) { return bernstein3(c0, c1, c2, c3, t); });
     }
