9e51f23347bc4701f5dbc1178866e51f326f6981
Aznes <timotheefageot@outlook.fr>
2025-07-18 10:40:04 +0200

    Draw circle!

--- src/main.cpp
+++ src/main.cpp
@@ -4,123 +4,52 @@
 #include <vector>
 #include <cmath>
 
-struct Particle {
-    glm::vec2 position;
-    glm::vec2 velocity;
-    float     radius = 0.005f;
-    glm::vec4 color  = glm::vec4(1.0f);
-};
+constexpr float PI = 3.14159265358979323846f;
+
+glm::vec2 circleCenterPosition  = glm::vec2(0.0f, 0.0f);
+float circleRadius  = 0.5f;
+
+int segmentsAmount = 100;
+float segmentsThickness = 0.01f;
+
+glm::vec2 bezier3(const glm::vec2& p0, const glm::vec2& p1, const glm::vec2& p2, const glm::vec2& p3, float t) {
+    float u = 1.0f - t;
+    float tt = t * t;
+    float uu = u * u;
+    float uuu = uu * u;
+    float ttt = tt * t;
+
+    glm::vec2 p = uuu * p0;
+    p += 3 * uu * t * p1;
+    p += 3 * u * tt * p2;   
+    p += ttt * p3;
+
+    return p;
+}
+
+void draw_parametric(std::function<glm::vec2(float)> const& parametric)
+{
+    glm::vec2 prev = parametric(0.0f);
+
+    for (int i = 1; i <= segmentsAmount; ++i) {
+        float t = float(i) / segmentsAmount;
+        glm::vec2 cur = parametric(t);
+        utils::draw_line(prev, cur, segmentsThickness, glm::vec4(1.0f));
+        prev = cur;
+    }
+}
 
 int main() {
     gl::init("Particules!");
     gl::maximize_window();
 
-    constexpr float PI = 3.14159265358979323846f;
-    constexpr float R = 0.3f;
-    constexpr float minDist = 0.02f;
-    constexpr int k = 30;
-
-    float cellSize = minDist / std::sqrt(2.0f);
-    int gridW = static_cast<int>(std::ceil(2 * R / cellSize));
-    int gridH = gridW;
-
-    glm::vec2 origin(0.0f, 0.0f);
-    glm::vec2 xEnd  (0.0f, 1.0f);
-    glm::vec2 yEnd  (1.0f, 0.0f);
-    glm::vec2 bx = xEnd - origin;
-    glm::vec2 by = yEnd - origin;
-    glm::vec2 center = origin + (bx + by) * 0.5f;
-
-    std::vector<int> grid(gridW * gridH, -1);
-    std::vector<glm::vec2> samples;
-    std::vector<int> active;
-
-    auto toGrid = [&](const glm::vec2& p) {
-        int gridX = static_cast<int>((p.x + R) / cellSize);
-        int gridY = static_cast<int>((p.y + R) / cellSize);
-
-        return std::make_pair(gridX, gridY);
-    };
-
-    glm::vec2 position0;
-
-    do {
-        float x = utils::rand(-R, R);
-        float y = utils::rand(-R, R);
-        position0 = glm::vec2(x, y);
-    } while (glm::length(position0) > R);
-
-    samples.push_back(position0);
-    active.push_back(0);
-    auto[gridX0, gridY0] = toGrid(position0);
-    grid[gridY0 * gridW + gridX0] = 0;
-
-    while (!active.empty()) {
-        int randomIndex = static_cast<int>(utils::rand(0.0f, (float)active.size()));
-
-        if (randomIndex == active.size()) randomIndex--;
-
-        int index = active[randomIndex];
-        bool found = false;
-
-        for (int i = 0; i < k; i++) {
-            float randomRadius = utils::rand(minDist, 2 * minDist);
-            float theta = utils::rand(0.0f, 1.0f) * 2.0f * PI;
-            glm::vec2 cand = samples[index] + glm::vec2(randomRadius * std::cos(theta), randomRadius * std::sin(theta));
-
-            if (glm::length(cand) > R) continue;
-
-            auto[gridX, gridY] = toGrid(cand);
-
-            if (gridX < 0 || gridX >= gridW || gridY < 0 || gridY >= gridH) continue;
-
-            bool ok = true;
-
-            for (int ix = std::max(0, gridX-2); ix <= std::min(gridW-1, gridX+2); ix++) {
-                for (int iy = std::max(0, gridY-2); iy <= std::min(gridH-1, gridY+2); iy++) {
-                    int sidx = grid[iy * gridW + ix];
-
-                    if (sidx != -1 && glm::length(cand - samples[sidx]) < minDist) {
-                        ok = false;
-                        break;
-                    }
-                }
-            }
-
-            if (!ok) continue;
-
-            samples.push_back(cand);
-            active.push_back(static_cast<int>(samples.size()) - 1);
-            grid[gridY * gridW + gridX] = static_cast<int>(samples.size()) - 1;
-            found = true;
-
-            break;
-        }
-
-        if (!found) {
-            active.erase(active.begin() + randomIndex);
-        }
-    }
-
-    std::vector<Particle> particles;
-
-    particles.reserve(samples.size());
-
-    for (auto& localPos : samples) {
-        glm::vec2 worldPos = center + bx * localPos.x + by * localPos.y;
-        Particle p;
-        p.position = worldPos;
-        particles.push_back(p);
-    }
-
     while (gl::window_is_open()) {
+        glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
-
-        utils::draw_line(origin, xEnd, 0.005f, glm::vec4(1,0,0,1));
-        utils::draw_line(origin, yEnd, 0.005f, glm::vec4(0,1,0,1));
-
-        for (auto& p : particles) {
-            utils::draw_disk(p.position, p.radius, p.color);
-        }
+    
+        draw_parametric([](float t) {
+            float angle = 2.0f * PI * t;
+            return circleCenterPosition + circleRadius * glm::vec2(std::cos(angle), std::sin(angle));
+        });
     }
 }
